d859c94071ffc8db8022257f24c10c8da069339e
fares <fares.bensaad@univ-constantine2.dz>
2022-05-12 13:55:02 +0400

    Update database config

--- config/server.js
+++ config/server.js
@@ -1,7 +1,14 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  proxy: true,
+  url: env("https://immense-oasis-56579.herokuapp.com/"),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array(
+      "30ijTkniyHu7zLp4yjgkng==,89YnkRUF5ORzUVoEryBlsg==,IiXZZBFlPSc2b5ecs22s+g==,XseXLd9ZzVDng/ZqnZWkMw=="
+    ),
   },
 });
+// API token N8yVQmR/VSaFdlv3knNVeg
+
+// admin jwtsecret 9XP6rtjUj9l8mBeZlvnetw
+
+// jwtsecret       JKKAFbz+w44RZxACsWzllQ
